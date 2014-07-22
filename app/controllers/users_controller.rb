c041fd99a1b8752dd731b41060068b2ce40f426a
D.Bagdasaryan <davo.bagdasaryan@gmail.com>
2014-07-22 12:55:26 +0200

    usuniecie branchy

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+#codding: UTF-8
+
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  #before_action :make_sure_its_mine
+  
+  def index
+    @user=User.all  
+  end
+
+  def show
+    @user=current_user
+  end
+
+  #private
+   # def make_sure_its_mine
+    #  unless current_user.id==params[:id].to_i
+     #   redirect_to root_url, alert: "To nie twoje konto!"
+      #end
+      #true
+    #end
+end
