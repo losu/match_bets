10ded812e77a78148c29dbdd896c28c30d765be2
Artur Dudzik <dudziaso@gmail.com>
2014-07-23 11:47:26 +0200

    Add test for Users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'should have attributes :admin, nickname, email' do 
+    expect(subject.attributes).to include('admin', 'nickname', 'email')
+  end
 end
