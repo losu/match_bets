01effc8e0774f60753e912f8144f61a99c192984
Artur Dudzik <dudziaso@gmail.com>
2014-07-24 14:47:01 +0200

    show group list in navbar and link to create group added to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :get_groupset_list
+    def get_groupset_list
+        @groupsets = Groupset.where(user_id: current_user.id)
+        @Users2 = User.all
+    end
+
 
   #=================DODANIE POLA DO REJESTRACJI DEVISE=============================
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
