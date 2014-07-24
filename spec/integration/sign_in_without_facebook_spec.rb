275ea2af1601da0f0cc6a03f1a2f81bc3584fd25
tytuspowroznik <tytus.powroznik@gmail.com>
2014-07-24 12:40:52 +0200

    capybara-stage1

--- spec/integration/sign_in_without_facebook_spec.rb
+++ spec/integration/sign_in_without_facebook_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
+include Capybara::DSL
 require 'capybara/rspec'
 
 describe "log in without facebook" do
 	before :each do
-  		User.new(:email => 'user@example.com', :password => 'cokolwiek')
+  		User.new(email: "user@example.com", password: "cokolwiek")
 	end
-it "signs me in " do
-	visit '/users/sign_in'
-    within ("#user") do
-    	fill_in 'Email', :with => 'user@example.com'
-      	fill_in 'Password', :with => 'password'
-      end
-      click_button 'Sign in'
-      expect(page).to have_content 'Dodaj mecz'
-  end
+  it "signs me in" do
+  	visit '/users/sign_in'
+      within ("#user") do
+      	fill_in 'Email', :with => 'user@example.com'
+        	fill_in 'Password', :with => 'password'
+        end
+        click_button 'Sign in'
+        expect(page).to have_content 'Dodaj mecz'
+    end
 end
