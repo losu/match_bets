28fc5fa5abd97b73343475c93988470eabd823ed
Emil <emil_ro@o2.pl>
2014-07-29 14:42:40 +0200

    routes and groups controller fix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,18 +38,6 @@ class GroupsController < ApplicationController
 		@group = Group.new
 	end
 
-	def add
-		@token = params[:invite_token]
-		if @token != nil
-     		org =  Invite.find_by_token(@token).group_id
-     		groupset = Groupset.new
-	 	  	groupset.user_id = resource.id
-	 		groupset.group_id = org
-	 		groupset.points_in_group = 0
-	 		groupset.save
-	 	end
-	end
-
 	def create
 		@group=Group.new(group_params)
 		@group.active = true
