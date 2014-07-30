c7e5194cd4545b15e583909dbf5e55d20cc1dd01
tytuspowroznik <tytus.powroznik@gmail.com>
2014-07-30 14:55:34 +0200

    scope in groupset

--- app/models/groupset.rb
+++ app/models/groupset.rb
@@ -1,5 +1,6 @@
 #encoding UTF-8
 class Groupset < ActiveRecord::Base
+  default_scope { order('points_in_group DESC') }
   validates :group_id, presence: true
   validates :user_id, presence: true
 	belongs_to :group
--- app/models/matchset.rb
+++ app/models/matchset.rb
@@ -1,6 +1,5 @@
 #encoding UTF-8
 class Matchset < ActiveRecord::Base
-  default_scope order('points_in_group DESC')
   validates :group_id, presence: true
   validates :match_id, presence: true
   belongs_to :group
