6de1b155a7ac637ebc50f7a489758a96f1df0cac
tytuspowroznik <tytus.powroznik@gmail.com>
2014-07-31 10:08:54 +0200

    failing tests fix

--- spec/integration/make_new_group_spec.rb
+++ spec/integration/make_new_group_spec.rb
@@ -4,7 +4,7 @@ require 'capybara/rspec'
 
 describe "make new group:" do
   before :each do
-    @user=User.create(email: "test@example.com", password: "qwerty12345")
+    @user=User.create(id: 777, email: "test@example.com", password: "qwerty12345", admin: true)
     visit '/users/sign_in'
     fill_in 'user_email',        :with => "test@example.com"
     fill_in 'user_password',     :with => "qwerty12345"
@@ -14,8 +14,12 @@ describe "make new group:" do
   it 'create new group' do
     visit '/groups/new'
     fill_in 'group_name',  :with => "uber grupa 9000"
-    print page.html
+    # print page.html
     find_button('Add!').click
+    # click_on("add_button")
+    # page.find("#add_button").click
+    # find('#add_button',:visible => true).click
+    # find("#add_button").click
     expect(page).to have_content ("Dodano grupę")
   end
 end
